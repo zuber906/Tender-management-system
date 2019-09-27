be1a64dc832e9acdb8222f984d3aefbbba936281
meer <meer@DESKTOP-1H02SQI>
2019-09-27 17:35:24 +0530

    user can update the details from this program

--- JDBCexample/src/example.java
+++ JDBCexample/src/example.java
@@ -1,4 +1,5 @@
-import java.sql.*;
+ import java.sql.*;
+ import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -9,16 +10,26 @@ public class example {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","TENDERSYSTEM","CRAZYminds1@");
+			Scanner sc = new Scanner(System.in);
+			String username = sc.next();
+			String password = sc.next();
+			PreparedStatement ps=conn.prepareStatement("INSERT INTO ADMIN(ADMIN_username,password) VALUES(?,?)");
+			ps.setString(1,username);
+			ps.setString(2, password);
+			int status=ps.executeUpdate();
 			Statement st = conn.createStatement();
-			st.executeUpdate("INSERT INTO ADMIN VALUES ('surya roa','dash435')");//admin login details
-			st.executeUpdate("INSERT INTO CONTRACTOR VALUES ('vijay surya roa','surya roa','dash435','surya1546@gmail.com','9987656655','male')");//as contractor credentials will be lenghtjy because to acess admin details ,might be some maintain privacy
-			st.executeUpdate("INSERT INTO OFFICIALS VALUES('vijay surya roa','assistant engineer','transport')");//officials means who are yet to work or involve in only known these details
+			ResultSet rs = st.executeQuery("SELECT name FROM ADMIN");
+			while(rs.next())
+			{
+				System.out.println(rs.getString("username"));
+			}
 			conn.close();
 		}
+
 		catch (Exception e) {
 			System.err.println("Got an exception! ");
 			
 		}
 	}
 
-}
+	}
